6a8e5fbc613abf9819f1a6058dbefebdb1c8782e
Shivakumar, Sudhir (sshivakumar) <sshivakumar@stubhub.com>
2015-08-01 00:49:57 -0700

    First upload
    First upload of project repository

---
+++ cloud/cloudConfig.js
@@ -0,0 +1,14 @@
+//Your Twilio account credentials
+exports.accountSid = 'AC207bd64e6720ad52c42848f3ab06759f';
+exports.authToken = '0b9e3722d9e4a4499dfb7c47ce8c3898';
+
+//A Twilio number you control
+exports.from = '+14083891814';
+
+//The message prepended to the text message link
+exports.message = 'Download the free StubHub app from: ';
+
+//Change this to be wherever you end up deploying your HTML files
+//You might also consider using a shortened link to save space in
+//your message.
+exports.downloadLink = 'http://shsmsapplinks.parseapp.com/download.html';
